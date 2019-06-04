284ab53e377129aef52e08db3a69e61231a196f0
song <740341854@qq.com>
2019-06-04 09:59:30 +0800

    添加init-ui.el

--- init.el
+++ init.el
@@ -6,23 +6,15 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'init-packages)
+(require 'init-ui)
 
 ;; 关闭滚动到底部的提示音
 (setq ring-bell-function 'ignore)
 
-;; 开启后默认全屏
-(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
 
 ;; 启用括号匹配
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
-
-;;当前行高亮
-(global-hl-line-mode 1)
-
-;; 安装主题
-(add-to-list 'my/packages 'monokai-theme)
-(load-theme 'monokai 1)
-
 
 (setq enable-recursive-minibuffers t)
 (global-set-key "\C-s" 'swiper)
@@ -42,30 +34,6 @@
 ;; 有修改自动加载
 (global-auto-revert-mode t)
 
-;; 关闭工具栏, tool-bar-mode 即为一个 Minor Mode
-(tool-bar-mode -1)
-
-;; 关闭文件滑动控件
-(scroll-bar-mode -1)
-
-;; 显示行号
-;; (global-linum-mode 1) ;; 这一个比较老, 效率不好
-(global-display-line-numbers-mode 1);
-
-;; 更改光标样式
-(setq-default cursor-type 'bar)
-
-;; 关闭启动帮助画面
-(setq inhibit-splash-screen 1)
-
-;; 更改显示字体大小 16pt
-(set-face-attribute 'default nil :height 110)
-(set-default-font "Consolas")
-(set-fontset-font "fontset-default" 'chinese-gbk "微软雅黑")
-
-(setq face-font-rescale-alist '(("宋体" . 1.1)
-                ("微软雅黑" . 1.0)
-                ))
 
 ;; 快速打开配置文件
 (defun open-init-file()
